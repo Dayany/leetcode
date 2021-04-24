12277a4a546eea09768ab380375df90575d6f038
Dayan Yamin <yamindayan@gmail.com>
2021-04-23 23:38:30 -0400

    Problem Solved: 1313

--- 1313. Decompress Run-Length Encoded List/main.js
+++ 1313. Decompress Run-Length Encoded List/main.js
@@ -5,7 +5,7 @@
 var decompressRLElist = function (nums) {
   let result = [];
 
-  for (let index = 0; index < nums.length; index + 2) {
+  for (let index = 0; index < nums.length; index += 2) {
     for (let i = 0; i < nums[index]; i++) {
       result.push(nums[index + 1]);
     }
