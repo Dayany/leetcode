d86772bcbb10071251fb887200bfb738788695b6
Dayan Yamin <yamindayan@gmail.com>
2021-04-25 15:50:48 -0400

    Problem Solved: 1773

---
+++ 1773. Count Items Matching a Rule/main.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[][]} items
+ * @param {string} ruleKey
+ * @param {string} ruleValue
+ * @return {number}
+ */
+var countMatches = function (items, ruleKey, ruleValue) {
+  let positionKey = [];
+  positionKey["type"] = 0;
+  positionKey["color"] = 1;
+  positionKey["name"] = 2;
+  let result = 0;
+  for (let i = 0; i < items.length; i++) {
+    if (items[i][positionKey[ruleKey]] === ruleValue) {
+      result++;
+    }
+  }
+};
