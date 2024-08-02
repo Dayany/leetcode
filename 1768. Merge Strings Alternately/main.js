d9fe7eb501b963bc3e8b95b77dabc935306c3aa1
Dayan Yamin <yamindayan@gmail.com>
2024-08-02 14:27:39 -0400

    Problem Solved 1768

--- 1768. Merge Strings Alternately/main.js
+++ 1768. Merge Strings Alternately/main.js
@@ -5,16 +5,18 @@
  */
 var mergeAlternately = function (word1, word2) {
   let result = "";
-  let biggestLength = word1.length > word2.length ? word1.length : word2.length;
+  let word1Pointer = 0;
+  let word2Pointer = 0;
 
-  for (let i = 0; i < biggestLength; i++) {
-    if (i < word1.length) {
-      result += word1.charAt(i);
+  while (word1Pointer < word1.length || word2Pointer < word2.length) {
+    if (word1Pointer < word1.length) {
+      result += word1.charAt(word1Pointer);
+      word1Pointer++;
     }
-    if (i < word2.length) {
-      result += word2.charAt(i);
+    if (word2Pointer < word2.length) {
+      result += word2.charAt(word2Pointer);
+      word2Pointer++;
     }
   }
-
   return result;
 };
